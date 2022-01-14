e8bc4f0ad3621e5be6d09469e0fb704a15737e6e
Manuel Lemus <manuellemuslemus23@gmail.com>
2022-01-14 14:25:47 -0800

    infraestructura de modelo, migracions, controladores y factories, todos son archivos bacios de momento

---
+++ database/factories/PayrollDetailFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PayrollDetail;
+use Faker\Generator as Faker;
+
+$factory->define(PayrollDetail::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
